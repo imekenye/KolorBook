166a296633842dce030d251d24791a2138ac1be6
imekenye <ismekenye@gmail.com>
2021-11-17 11:02:55 +0300

    add svg icons

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,1 +1,4 @@
+export { default as ArtOne } from './ArtOne'
+export { default as ArtTwo } from './ArtTwo'
+export { default as ColorPallette } from './ColorPallette'
 export { default as Logo } from './Logo'
