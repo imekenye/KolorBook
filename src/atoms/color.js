5ba3de20cb36331d1c371aee8f7e38108013b6f7
imekenye <ismekenye@gmail.com>
2021-11-18 15:46:00 +0300

    add atom folder for use with recoil

--- src/atoms/color.js
+++ src/atoms/color.js
@@ -4,3 +4,13 @@ export const colorsState = atom({
   key: 'colorsState',
   default: [],
 });
+
+export const fillColorState = atom({
+  key: 'fillColorState',
+  default: Array(8).fill('white'),
+});
+
+export const currentColorState = atom({
+  key: 'currentColorState',
+  default: '#A84CA0',
+});
